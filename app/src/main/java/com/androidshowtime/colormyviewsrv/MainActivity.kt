f8e5525e2444518145ac87ef3c165a6b2ceeada5
Antony <vontonnie@gmail.com>
2020-11-13 17:15:19 +0300

    Add Click Handler Code

--- app/src/main/java/com/androidshowtime/colormyviewsrv/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/colormyviewsrv/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.androidshowtime.colormyviewsrv
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
+    private fun makeColored(view:View) {
+
+        when(view.id){
+
+
+
+            //Boxes using Color class color for background
+            R.id.box_one_text-> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text->view.setBackgroundColor(Color.GRAY)
+
+
+
+
+            //Boxes using Android color resources for background
+            R.id.box_three_text->view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text->view.setBackgroundResource(android.R.color.holo_green_dark)
+
+            else -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+        }
+
+    }
+
+
 }
