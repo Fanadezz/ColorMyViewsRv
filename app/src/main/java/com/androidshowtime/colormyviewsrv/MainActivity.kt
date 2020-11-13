34ba9ccc5fb77e8b225d67482998e0e88f3244a5
Antony <vontonnie@gmail.com>
2020-11-13 17:25:08 +0300

    Add Click Logic

--- app/src/main/java/com/androidshowtime/colormyviewsrv/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/colormyviewsrv/MainActivity.kt
@@ -1,36 +1,33 @@
 package com.androidshowtime.colormyviewsrv
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+setListeners()
     }
 
 
-    private fun makeColored(view:View) {
+    private fun makeColored(view: View) {
 
-        when(view.id){
-
+        when (view.id) {
 
 
             //Boxes using Color class color for background
-            R.id.box_one_text-> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text->view.setBackgroundColor(Color.GRAY)
-
-
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
 
             //Boxes using Android color resources for background
-            R.id.box_three_text->view.setBackgroundResource(android.R.color.holo_green_light)
-            R.id.box_four_text->view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
 
             else -> view.setBackgroundResource(android.R.color.holo_green_light)
 
@@ -39,6 +36,17 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    fun setListeners() {
 
+        val views = mutableListOf(
+                box_one_text, box_two_text, box_three_text, box_four_text, box_five_text)
+
+
+        for (view in views){
+
+
+            view.setOnClickListener { makeColored(it) }
+        }
+    }
 
 }
